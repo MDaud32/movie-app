1b8609068085bae6dc79890f4635b7d2b38f46a3
MDaud32 <theprofessor543@gmail.com>
2024-08-31 15:39:43 +0500

    testing

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,17 +1,17 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-function Error({ error, reset }) {
-  useEffect(() => {
-    console.log(error);
-  }, [error]);
-  return (
-    <div>
-      <h1>somethings wrong</h1>
-      <button onClick={() => reset()}>try again</button>
-    </div>
-  );
-}
+// function Error({ error, reset }) {
+//   useEffect(() => {
+//     console.log(error);
+//   }, [error]);
+//   return (
+//     <div>
+//       <h1>somethings wrong</h1>
+//       <button onClick={() => reset()}>try again</button>
+//     </div>
+//   );
+// }
 
-export default Error;
+// export default Error;
